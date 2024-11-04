734496a5ccc9e3e752d42dd5e656b19ad4678aa4
tommyfq <t.quiko@bumi.ai>
2024-11-05 01:10:06 +0700

    :rocket: finishing items api

--- app/controllers/item.controller.js
+++ app/controllers/item.controller.js
@@ -1,13 +1,15 @@
 const moment = require('moment')
+const xlsx = require('xlsx');
+const path = require("path");
+const fs = require("fs");
 const db = require("../models");
 const Op = db.Sequelize.Op;
 const { sequelize, Sequelize } = require("../models");
 const Items = db.items;
+const { createPagination, createPaginationNoData } = require("../helpers/pagination");
 
 const list = (req,res) => {
   console.log("===ASSET_LIST===")
-  /* search by company name */
-  /* search by agent name */
 
   /* 
     search_company_name
@@ -18,8 +20,8 @@ const list = (req,res) => {
     order
   */
   console.log(req.body);
-  var page = req.body.page;
-  var page_length = 20; //default 20
+  let page = parseInt(req.body.page, 10);
+  var page_length = req.body.items_per_page; //default 20
   var column_sort = "id";
   var order = "asc"
 
@@ -39,24 +41,22 @@ const list = (req,res) => {
   
   param_order = [column_sort,order];
   
-  if(req.body.hasOwnProperty("search_brand")){
-    if(req.body.search_brand != ""){
+  if(req.body.hasOwnProperty("search")){
+    if(req.body.search != ""){
       where_query = {
         ...where_query,
-        brand: {
-          [Op.iLike]: '%'+req.body.search_brand+'%'
-        }
-      }
-    }
-  }
-
-  if(req.body.hasOwnProperty("search_model")){
-    if(req.body.search_model != ""){
-      where_query = {
-        ...where_query,
-        model: {
-          [Op.iLike]: '%'+req.body.search_model+'%'
-        }
+        [Op.or]: [
+          {
+              brand: {
+                  [Op.iLike]: `%${req.body.search}%`
+              }
+          },
+          {
+            model: {
+                [Op.iLike]: `%${req.body.search}%`
+            }
+          }
+      ]
       }
     }
   }
@@ -77,20 +77,25 @@ const list = (req,res) => {
       raw:true
   })
   .then(result => {
-      if(result.count == 0){
-          res.status(200).send({
-              message:"No Data Found in Items",
-              data:result.rows,
-              total:result.count
-          })
-      }else{
-    
-        res.status(200).send({
-            message:"Success",
-            data:result.rows,
-            total:result.count
-        });
-      }
+    const total_count = result.count; // Total number of items
+    const total_pages = Math.ceil(total_count / page_length)
+
+    if (result.count === 0) {
+      res.status(200).send({
+        message: "No Data Found in Items",
+        data: result.rows,
+        payload: createPaginationNoData(page, total_pages, page_length, 0)
+      });
+    } else {
+      
+      res.status(200).send({
+        message: "Success",
+        data: result.rows,
+        payload: {
+          pagination: createPagination(page, total_pages, page_length, result.count)
+        }
+      });
+    }
   });
 };
 
@@ -271,11 +276,183 @@ const listModel = (req,res) => {
     });
   };
 
+const upload = async(req, res) => {
+
+  try{
+    if (req.file == undefined) {
+      return res.status(200).send({
+        is_ok: false,
+        message: "Please upload a excel file!"});
+    }
+    let dir = __basedir + "/uploads/excel/" + req.file.filename;
+    const file = xlsx.readFile(dir);
+    
+    const sheets = file.SheetNames
+
+    var result = [];
+    
+    const t = await sequelize.transaction();
+  
+      let temp = xlsx.utils.sheet_to_json(file.Sheets['item'])
+      
+      for(let j = 0; j < temp.length; j++){
+        var resp = null;
+        resp = await updateOrCreate(j,temp[j],t);
+
+        result.push(resp);
+      }
+    
+    fs.readdir(__basedir + "/uploads/excel/", (err, files) => {
+      if (err) throw err;
+    
+      for (const file of files) {
+        fs.unlink(path.join(__basedir + "/uploads/excel/", file), err => {
+          if (err) throw err;
+        });
+      }
+    });
+
+    await t.commit();
+    return res.status(200).send({
+      is_ok: true,
+      message: "Successfully Upload : " + req.file.originalname,
+      data:result
+    });
+
+    }catch(error){
+      console.log(error.toString());
+      await t.rollback();
+      return res.status(200).send({
+        is_ok: false,
+        message: "Could not upload the file: " + req.file.originalname,
+        error:error.toString()
+      });
+    }
+} 
+
+const updateOrCreate = async(i,row,t)=>{
+  try{
+    if(!row.hasOwnProperty('Brand')) {
+      return {is_ok:false,message:"Brand is blank at row "+(i+1)}
+    }
+
+    if(!row.hasOwnProperty('Model')) {
+      return {is_ok:false,message:"DC Name is blank at row "+(i+1)}
+    }
+
+
+    if(!row.hasOwnProperty('Is Active')) {
+      return {is_ok:false,message:"Is Active is blank at row "+(i+1)}
+    }
+
+
+    const existItems = await Items.findOne({
+      where:{
+        brand:row["Brand"],
+        model:row["Model"]
+      },
+      transaction: t
+    })
+
+    if(!existItems){
+      return {is_ok:false,message:"Item is not exist at row "+(i+1)}
+    }
+
+    console.log(row["Is Active"])
+
+    var storeData = {
+      brand:row["Brand"],
+      model:row["Model"],
+      is_active:row["Is Active"] == 'TRUE' || row["Is Active"] == true ? true : false,
+    }
+
+    console.log(storeData, existItems)
+  
+    if(existItems){
+
+      let hasChanged = Object.keys(storeData).some(key => {
+        // Ensure to handle the case where the key may not exist on existDC
+        return storeData[key] !== existItems[key];
+      });
+  
+      if (!hasChanged) {
+        return { is_ok:false, message: 'No changes detected, update skipped.' };
+      }
+      
+      await Items.update(storeData,
+        {
+          where:{
+            id:existItems.id
+          },
+          transaction:t
+        }
+      )
+      return {is_ok:true,message:`${storeData.brand} ${storeData.model} successfully update at row ${(i+1)}`}
+    }else{
+      await Items.create(storeData,{transaction:t})
+      return {is_ok:true,message:`${storeData.brand} ${storeData.model} successfully insert at row ${(i+1)}`}
+    }
+  
+  }catch(error){
+    return {is_ok:false,message:error.toString()};
+  }
+}
+
+const download = async(req, res) => {
+
+  try {
+    // Fetch all data from your data collection
+    const result = await Items.findAll({
+      attributes:[
+        'brand',
+        'model',
+        'is_active',
+        //[Sequelize.col('company.company_code'), 'company_code']
+      ],
+      // You can specify any options here if needed
+    });
+
+    const formattedResult = result.map((item, index) => {
+
+      return {
+      No: index + 1, // Incremental number
+      'Brand':item.brand,
+      'Model': item.model, // Use the alias for dc_name
+      'Is Active': item.is_active ? 'TRUE' : 'FALSE',
+      }
+    });
+
+    // Create a new workbook
+    const workbook = xlsx.utils.book_new();
+
+    // Convert data to a worksheet
+    const worksheet = xlsx.utils.json_to_sheet(formattedResult);
+
+    // Append the worksheet to the workbook
+    xlsx.utils.book_append_sheet(workbook, worksheet, 'item');
+
+    // Create a buffer to write the workbook
+    const excelBuffer = xlsx.write(workbook, { bookType: 'xlsx', type: 'buffer' });
+
+    // Set the response headers for file download
+    res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+    res.setHeader('Content-Disposition', 'attachment; filename="item_data.xlsx"');
+
+    // Send the Excel buffer as a response
+    res.send(excelBuffer);
+  } catch (error) {
+    console.error('Error generating Excel file:', error);
+    res.status(500).send('Internal Server Error');
+  }
+}
+
 module.exports = {
     create,
     list,
     detail,
     update,
     listBrand,
-    listModel
+    listModel,
+    download,
+    upload
 }
--- app/routes/items.route.js
+++ app/routes/items.route.js
@@ -1,40 +1,52 @@
-//const { authJwt, upload } = require("../middleware");
+const { authJwt, uploadExcel } = require("../middleware");
 const controller = require("../controllers/item.controller");
 
 module.exports = function(app) {
   app.post(
     "/api/item/create",
-    [],
+    [authJwt.verifyToken],
     controller.create
   );
 
   app.post(
     "/api/item/list",
-    [],
+    [authJwt.verifyToken],
     controller.list
   );
 
   app.get(
     "/api/item/detail/:id",
-    [],
+    [authJwt.verifyToken],
     controller.detail
   );
 
   app.post(
     "/api/item/update",
-    [],
+    [authJwt.verifyToken],
     controller.update
   );
 
   app.get(
     "/api/item/list-brand",
-    [],
+    [authJwt.verifyToken],
     controller.listBrand
   );
 
   app.post(
     "/api/item/list-model",
-    [],
+    [authJwt.verifyToken],
     controller.listModel
+  );
+
+  app.get(
+    "/api/item/download",
+    [authJwt.verifyToken],
+    controller.download
+  );
+
+  app.patch(
+    "/api/item/upload",
+    [authJwt.verifyToken, uploadExcel.single('file')],
+    controller.upload
   );
 };
